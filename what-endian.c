692c70f49b8861aecc31b911f45221ad4191c89e
*Pika <petachew@uw.edu>
2020-04-17 18:24:00 -0700

    Update what-endian.c

--- what-endian.c
+++ what-endian.c
@@ -6,9 +6,8 @@ int main()
    unsigned int i = 1; 
    char *c = (char*)&i; 
    if (*c)     
-       printf("Little endian"); 
+       printf("Little endian\n"); 
    else
-       printf("Big endian"); 
-   getchar(); 
+       printf("Big endian\n"); 
    return 0; 
 } 
